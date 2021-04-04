f633d649120683222b53019ac124b1015c28a150
msya3 <msya3github@gmail.com>
2021-04-04 22:29:27 +0900

    This is the second commit for AtCoder

--- AlgolithmKentei_5/Bingo.py
+++ AlgolithmKentei_5/Bingo.py
@@ -25,28 +25,23 @@ for _ in range(0, N):
     # bがビンゴカードに書かれているか調べる
     for i in range(0, 3):
         for j in range(0, 3):
-
             if A[i][j] == b:
                 M[i][j] = True
-
 bingo = False
 
-for i in range(0,3):
 
+# 以降はビンゴかどうかを確認する
+for i in range(0,3):
     if M[i][0] and M[i][1] and M[i][0]:
         bingo = True
-
 for i in range(0,3):
-
     if M[0][i] and M[1][i] and M[2][i]:
         bingo = True
-
-
 if M[0][0] and M[1][1] and M[2][2]:
     bingo = True
-
 if M[0][2] and M[1][1] and M[2][0]:
     bingo = True
+
 
 if bingo:
     print("Yes")
